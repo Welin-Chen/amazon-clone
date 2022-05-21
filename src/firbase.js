5b864a9b9884f537a5a5df35ee1df88dd6c3d181
welin <a0929015591@gmail.com>
2022-05-21 23:08:01 +0800

    1st commit- Navbar

---
+++ src/firbase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD4uK7ZbwnNwhDKOJ2vvTyH9MnTumFlNSg",
+  authDomain: "challenge-5c0c2.firebaseapp.com",
+  projectId: "challenge-5c0c2",
+  storageBucket: "challenge-5c0c2.appspot.com",
+  messagingSenderId: "884371998930",
+  appId: "1:884371998930:web:3ea64319cbf3d4c1f0850b",
+  measurementId: "G-FWEH3CMHBS",
+};
